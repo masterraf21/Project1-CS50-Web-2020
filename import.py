3eeb45ab75384859476f0b499415b7d6f9622237
masterraf21 <masterraf21>
2020-05-25 18:21:38 +0700

    fix import and add logging

--- import.py
+++ import.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import logging
 
 from time import time
 from datetime import datetime
@@ -14,11 +15,8 @@ if not os.getenv("DATABASE_URL"):
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
-def load_data(file_name):
-    file = open(file_name, mode='r')
-    reader = csv.reader(file)
-    return reader
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 
 def main():
@@ -31,8 +29,10 @@ def main():
     db.execute("CREATE TABLE reviews(id serial PRIMARY KEY, rating DECIMAL DEFAULT '0', review VARCHAR NOT NULL, user_id INTEGER references users NOT NULL, book_id INTEGER references books NOT NULL)")
 
     # load data from csv
-    data = load_data("books.csv")
-    for isbn, title, author, year in data:
+    file = open("books.csv", mode='r')
+    reader = csv.reader(file)
+    next(reader, None)
+    for isbn, title, author, year in reader:
         db.execute("INSERT INTO books(isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
                    {"isbn": isbn, "title": title, "author": author, "year": year})
 

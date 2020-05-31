e2c0019a98fb6d1021b3fc6e50e8479b4ddfdc92
masterraf21 <masterraf21@gmail.com>
2020-05-31 18:18:46 +0700

    add more

--- app/routes.py
+++ app/routes.py
@@ -8,10 +8,6 @@ from werkzeug.urls import url_parse
 
 # Add decorator : @login_required for protecting some routes from anonymous user
 
-# TODO add Search
-# TODO add Review
-# TODO create book page
-# TODO get review from Goodreads
 # TODO API for books by isbn
 
 
@@ -65,21 +61,53 @@ def register():
     return render_template('register.html', form=form, title='Register')
 
 
-@app.route('/search/<int:isbn>', methods=['GET', 'POST'])
+@app.route('/search', methods=['GET', 'POST'])
 @login_required
-def books(isbn):
-    # Create form 
+def search():
+    # Create form
     form = SearchForm()
-    
+    # get search query
+    session['searchQuery'] = form.search.data
+    # if form submitted from POST
+    if form.validate_on_submit():
+        if form.type.data == 'title':
+            books = Book.getBook_title(title=form.search.data)
+        elif form.type.data == 'author':
+            books = Book.getBook_author(author=form.search.data)
+        elif form.type.data == 'isbn':
+            books = Book.getBook_isbn(isbn=form.search.data)
+        # pass in the books list to sesion var
+        # will be accessed later
+        session['booksQuery'] = books
+        return redirect(url_for('searchResult'))
+
+    # render template for GET
+    return render_template('search.html', form=form)
+
+
+@app.route('/search_result', methods=['GET'])
+@login_required
+def searchResult():
+    # Get query and list of books
+    query = session['searchQuery']
+    # list of books JSON
+    book_list = session['booksQuery']
+    # convert list of books json to books object
+    books = list()
+    for b in book_list:
+        books.append(Book.getBookfromJSON(b))
+    # Render search results
+    return render_template('search_result.html', query=query, books=books)
+
 
 @app.route('/revsubmit', methods=['POST'])
 @login_required
 def revsubmit():
     # Get session's book json
-    temp = session['books']
+    temp = session['book']
     # Create a book object
     book = Book.getBookfromJSON(temp)
-    return render_template('review_message.html', status=session['reviewStatus'], book=book, )
+    return render_template('review_message.html', status=session['reviewStatus'], book=book)
 
 
 @app.route('/book/<int:book_id>', methods=['GET', 'POST'])
@@ -94,25 +122,25 @@ def books(book_id):
         # if available create a new book object from JSON
         book_json = json.loads(session['book'])
         book = Book.getBookfromJSON(book_json)
-        
+
     # Get user's review
-    ureview = Review.getReview_uid_bookid(current_user.id,book_id)
+    ureview = Review.getReview_uid_bookid(current_user.id, book_id)
     # Get book's review and count
     breview = Review.getReview_bookid(book_id)
     if breview != None:
         breview_count = 0
     else:
-        breview_count =len(breview)
+        breview_count = len(breview)
     # Count book's rating
     if breview_count == 0:
         breview_avgrating = 0
     else:
         breview_avgrating = Review.getAvgRating(breview)
-    
+
     # Get goodreads object
     greadsObject = Goodreads(book.isbn)
     greadsObject.getGoodreadsData()
-  
+
     # Set form review
     form = ReviewForm()
     # Check if POST is successful
@@ -120,13 +148,19 @@ def books(book_id):
         # Check if user already review the book
         if ureview == None:
             # Create review from POST
-            Review.create(form.rating.data,form.review.data,current_user.id,book.id)
+            Review.create(form.rating.data, form.review.data,
+                          current_user.id, book.id)
             # Set session var review
             session['reviewStatus'] = 1
-            return redirect(url_for('revsubmit'))
         else:
             session['reviewStatus'] = 0
-            return redirect(url_for('revsubmit'))
+
+        return redirect(url_for('revsubmit'))
+
 
     return render_template('book.html', book=book, form=form, greads=greadsObject, avgRating=breview_avgrating, ratingCount=breview_count)
 
+@app.route('/api/<isbn>')
+@login_required
+def api(isbn):
+    pass
